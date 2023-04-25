f4401fdb16491cefb3cb3f9c382affa96f1d47da
giaenuuu <admin@gruee.ch>
2023-04-25 11:29:03 +0200

    build and configure web project
    #2

---
+++ server/Web/Controllers/ServerController.cs
@@ -0,0 +1,14 @@
+﻿namespace Web.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("servers")]
+public class ServerController : ControllerBase
+{
+    [HttpGet]
+    public string GetAll()
+    {
+        return "Hello World";
+    }
+}
