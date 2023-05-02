d9db169c958aa0a7ce51d52d27d67a973ed8c3dc
giaenuuu <admin@gruee.ch>
2023-05-02 09:00:34 +0200

    chore update
    #2

--- client/main.js
+++ client/main.js
@@ -6,7 +6,7 @@ function createWindow() {
     win = new BrowserWindow({ width: 800, height: 600 });
     // load the dist folder from Angular
     win.loadURL(url.format({
-        pathname: "http://localhost:4200/",
+        pathname: path.join(__dirname, "/dist/index.html"),
         protocol: "file:",
         slashes: true
     }));
