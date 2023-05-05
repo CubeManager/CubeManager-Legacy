dc7d4a6e1e6a968e4f7fb5160a51ab20bfefa901
Jakub Walczak <jakub.walczak1@swisscom.com>
2023-05-05 16:32:27 +0200

    #16 first backend changes, also trying to get the system data

--- server/Web/Controllers/ServerController.cs
+++ server/Web/Controllers/ServerController.cs
@@ -1,6 +1,7 @@
 ﻿namespace Web.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 [ApiController]
 [Route("servers")]
@@ -9,6 +10,11 @@ public class ServerController : ControllerBase
     [HttpGet]
     public string GetAll()
     {
-        return "Hello World";
+            Process[] localAll = Process.GetProcesses();
+            Console.Write(localAll);
+            // foreach (Process server in localAll) {
+            // }
+            // return string.Join(", ", (object[])localAll);
+            return string.Join("/n", (object[])(typeof(Process).GetProperties()));
     }
 }
