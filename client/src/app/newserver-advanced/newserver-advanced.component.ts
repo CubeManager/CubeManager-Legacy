2e98b27ea52938e4dae1eaacbe2ac03e0602a6f5
Andreas Hofmann <aho129470@stud.gibb.ch>
2023-05-13 18:22:21 +0200

    add newserver advanced view

---
+++ client/src/app/newserver-advanced/newserver-advanced.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newserver-advanced',
+  templateUrl: './newserver-advanced.component.html',
+  styleUrls: ['./newserver-advanced.component.scss']
+})
+export class NewserverAdvancedComponent {
+  cancel() {
+
+  }
+
+  create() {
+    
+  }
+}
