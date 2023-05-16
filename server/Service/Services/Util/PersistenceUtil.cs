2f72787fafef1fe964cbf1398c2b7c037dadbe2a
giaenuuu <admin@gruee.ch>
2023-05-16 08:50:41 +0200

    add utils
    #5

---
+++ server/Service/Services/Util/PersistenceUtil.cs
@@ -0,0 +1,30 @@
+﻿namespace Service.Services.Util;
+
+using Service.InputModels;
+using Service.Services.Util.Util;
+
+public static class PersistenceUtil
+{
+    public static string GetServerPath(string serverName)
+    {
+        return Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            $"CubeManager\\{serverName}\\"
+        );
+    }
+
+    public static string GetJarFileName(ServerInputModel serverInput)
+    {
+        return $"{serverInput.serverType.ToString().ToLower()}-{serverInput.exactVersion}.jar";
+    }
+
+    public static string GetJarFileName(string serverName)
+    {
+        var cubeManagerConfig = CubeManagerConfigUtil.GetCubeManagerConfig(serverName);
+        if (!string.IsNullOrEmpty(cubeManagerConfig.jarFile))
+        {
+            return cubeManagerConfig.jarFile;
+        }
+        throw new Exception("No jar filename found");
+    }
+}
---
+++ server/Service/Services/Util/ServerProcessUtil.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Diagnostics;
+
+namespace Service.Services.Util;
+
+public static class ServerProcessUtil
+{
+    public static Process StartServerProcess(ProcessStartInfo startProcessInfo)
+    {
+        Process serverProcess = new Process();
+        serverProcess.StartInfo = startProcessInfo;
+        serverProcess.Start();
+        return serverProcess;
+    }
+
+    public static ProcessStartInfo CreateServerProcessStartInfo(string serverPath, string serverJarFile, int maxMemory)
+    {
+        ProcessStartInfo processStartInfo = new ProcessStartInfo();
+        processStartInfo.RedirectStandardInput = true;
+        processStartInfo.RedirectStandardOutput = true;
+        processStartInfo.WorkingDirectory = serverPath;
+        processStartInfo.FileName = "java";
+        processStartInfo.Arguments = $"-Xmx{maxMemory}M -jar {Path.Combine(serverPath, serverJarFile)} nogui";
+        return processStartInfo;
+    }
+}
