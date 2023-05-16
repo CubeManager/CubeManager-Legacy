044fdd9746b4eaaaf3db46750b6729297c0a1cef
giaenuuu <admin@gruee.ch>
2023-05-16 08:44:49 +0200

    add background service manager
    #5

---
+++ server/Service/BackgroundServices/BackgroundServiceManager.cs
@@ -0,0 +1,45 @@
+﻿namespace Service.BackgroundServices;
+
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Hosting;
+using Service.Hubs;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+public static class BackgroundServiceManager
+{
+    private static readonly ConcurrentDictionary<string, IHostedService> BackgroundServices =
+    new ConcurrentDictionary<string, IHostedService>();
+
+    public static void StartNewBackgroundService(IHubContext<ConsoleHub> hubContext, Process serverProcess, string serverName)
+    {
+        var serverOutputSenderService = new ServerOutputSenderService(
+            hubContext: hubContext,
+            serverProcess: serverProcess,
+            serverName: serverName
+        );
+
+        AddBackgroundService(serverOutputSenderService);
+    }
+
+    public static void AddBackgroundService(IHostedService service)
+    {
+        BackgroundServices.TryAdd(service.GetType().Name, service);
+    }
+
+    public static void RemoveBackgroundService(IHostedService service)
+    {
+        BackgroundServices.TryRemove(service.GetType().Name, out _);
+    }
+
+    public static async Task StopAllBackgroundServicesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var backgroundService in BackgroundServices.Values)
+        {
+            await backgroundService.StopAsync(cancellationToken);
+        }
+    }
+}
+
