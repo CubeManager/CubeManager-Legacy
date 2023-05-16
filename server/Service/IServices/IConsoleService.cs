23e438e1a03440201cf3919a333741525c84d326
giaenuuu <admin@gruee.ch>
2023-05-16 08:45:14 +0200

    add interfaces for needed services
    #5

---
+++ server/Service/IServices/IConsoleService.cs
@@ -0,0 +1,14 @@
+﻿namespace Service.IServices;
+
+using System.Net.WebSockets;
+
+public interface IConsoleService
+{
+    /// <summary>
+    /// Redirects the output stream of a running server to the a websocket
+    /// </summary>
+    /// <param name="webSocket">The websocket to redirect to</param>
+    /// <param name="serverName">The server to redirext from</param>
+    /// <returns></returns>ddf
+    public Task RedirectConsoleStream(WebSocket webSocket, string serverName);
+}
---
+++ server/Service/IServices/IServerReadService.cs
@@ -0,0 +1,12 @@
+﻿namespace Service.IServices;
+
+using Service.InputModels;
+
+public interface IServerReadService
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="serverInput"></param>
+    public ServerViewModel GetServer(string serverName);
+}
