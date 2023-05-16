0a03b39c211b5d22185805260583c75f4034b286
giaenuuu <admin@gruee.ch>
2023-05-16 08:43:32 +0200

    feat: add process managment service
    #5

---
+++ server/Service/IServices/IProcessManagementService.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace Service.IServices;
+
+public interface IProcessManagementService
+{
+    public Dictionary<string, Process> ActiveServers { get; set; }
+    public Process Start(string serverName);
+    public void StopAll();
+    public void Stop(string serverName);
+    public void Restart(string serverName);
+    public void RestartAll();
+}
---
+++ server/Service/Services/ProcessManagementService.cs
@@ -0,0 +1,76 @@
+﻿using Service.IServices;
+using Service.Services.Util;
+using Service.Services.Util.Util;
+using System.Diagnostics;
+
+namespace Service.Services;
+
+public class ProcessManagementService : IProcessManagementService
+{
+    public Dictionary<string, Process> ActiveServers { get; set; }
+
+    public ProcessManagementService()
+    {
+        ActiveServers = new Dictionary<string, Process>();
+    }
+
+    public Process Start(string serverName)
+    {
+        if (ActiveServers.ContainsKey(serverName))
+        {
+            throw new Exception($"Server \"{serverName}\"is already running");
+        }
+
+        var serverConfig = CubeManagerConfigUtil.GetCubeManagerConfig(serverName);
+        var processStartInfo = ServerProcessUtil.CreateServerProcessStartInfo(
+            PersistenceUtil.GetServerPath(serverName), 
+            serverConfig.jarFile!, 
+            serverConfig.maxMemory);
+        var process = ServerProcessUtil.StartServerProcess(processStartInfo);
+
+        ActiveServers.Add(serverName, process);
+
+        return process;
+    }
+
+    public async void Restart(string serverName)
+    {
+        var process = ActiveServers[serverName];
+        await KillProcess(process);
+        process.Start();
+    }
+
+    public async void RestartAll()
+    {
+        foreach (Process process in ActiveServers.Values)
+        {
+            await KillProcess(process);
+            process.Start();
+        }
+
+    }
+
+    public async void Stop(string serverName)
+    {
+        var process = ActiveServers[serverName];
+        await KillProcess(process);
+        ActiveServers.Remove(serverName);
+    }
+
+    public async void StopAll()
+    {
+        foreach((string serverName, Process process) in ActiveServers)
+        {
+            await KillProcess(process);
+            ActiveServers.Remove(serverName);
+        }
+
+    }
+
+    private async Task KillProcess(Process process)
+    {
+        process.Kill();
+        await process.WaitForExitAsync();
+        return;
+    }
+}
