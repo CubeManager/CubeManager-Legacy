c7d3af4aef8876ad6ccb3138297bc8cae15ceb37
giaenuuu <admin@gruee.ch>
2023-06-05 21:07:13 +0200

    changes

--- server/Service/InputModels/ServerInputModel.cs
+++ server/Service/InputModels/ServerInputModel.cs
@@ -4,11 +4,9 @@ using Domain.Enums;
 
 public class ServerInputModel
 {
-    public int? serverId { get; set; }
     public string serverName { get; set; }
     public ServerType serverType { get; set; }
     public string exactVersion { get; set; }
-    public string? imageURL { get; set; }
     public int maxMemory { get; set; }
     public ServerPropertiesInputModel? serverProperties { get; set; }
 }
