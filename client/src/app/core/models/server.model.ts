1e58521fb44bf34d46f5d8db3b81bc702368b0e4
Bryan Schmid <bs@nexplore.ch>
2023-06-06 15:12:30 +0200

    merci giaenuuuuu

--- client/src/app/core/models/server.model.ts
+++ client/src/app/core/models/server.model.ts
@@ -2,13 +2,14 @@ import { ServerType } from "../enums/server-type.enum";
 import { ServerProperties } from "./server-properties.model";
 
 export interface Server {
-  serverName: string | null;
-  isRunning: boolean;
-  cpu: number;
-  memory: number;
-  currentPlayers: number;
-  serverType: ServerType;
-  exactVersion: string | null;
-  maxMemory: number;
+  serverName: string | null | undefined;
+  isRunning?: boolean;
+  cpu?: number;
+  memory?: number;
+  currentPlayers?: number;
+  serverType: ServerType | null | undefined;
+  exactVersion?: string | null;
+  maxMemory?: number;
   serverProperties: ServerProperties;
+  serverFileName?: string;
 }
